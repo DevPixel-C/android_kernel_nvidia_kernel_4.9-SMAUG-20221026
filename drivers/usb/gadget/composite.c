e9cb2a2052bf4e59ab7d381816f15cc5bb0c4652
Henry Lin <henryl@nvidia.com>
2015-11-09 18:48:22 +0800

    usb: gadget: avoid too large bMaxPower value
    encode_bMaxPower() uses DIV_ROUND_UP to encode MaxPower and may cause
    encoded value larger than maximum values defined in USB spec: 900 ms for
    USB 3.0 and 500 ms for USB 2.0 devices. Too large bMaxPower value will
    cause Linux host fail to choose configuration and report insufficient
    available bus power error.
    
    Bug 200152469
    
    Change-Id: I29480a5355881d82bcda6b860431280558c23685
    Signed-off-by: Henry Lin <henryl@nvidia.com>
    Reviewed-on: http://git-master/r/830705
    (cherry picked from commit 24521e09e1abcf07c030da00e2f5f280a0813bb0)
    Reviewed-on: http://git-master/r/1166737
    (cherry picked from commit 36a3801647aa7c929b91fa42d05eebc84ffe2180)
    Reviewed-on: http://git-master/r/1495223
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: svccoveritychecker <svccoveritychecker@nvidia.com>
    Reviewed-by: WK Tsai <wtsai@nvidia.com>
    Reviewed-by: Ashutosh Jha <ajha@nvidia.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -489,13 +489,13 @@ static u8 encode_bMaxPower(enum usb_device_speed speed,
 	if (!val)
 		return 0;
 	if (speed < USB_SPEED_SUPER)
-		return min(val, 500U) / 2;
+		return min(DIV_ROUND_UP(val, 2), 0xfa);
 	else
 		/*
 		 * USB 3.x supports up to 900mA, but since 900 isn't divisible
 		 * by 8 the integral division will effectively cap to 896mA.
 		 */
-		return min(val, 900U) / 8;
+		return min(DIV_ROUND_UP(val, 8), 0x70);
 }
 
 static int config_buf(struct usb_configuration *config,
