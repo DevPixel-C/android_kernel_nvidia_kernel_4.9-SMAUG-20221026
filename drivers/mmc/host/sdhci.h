51ff6e15c912174a1d59953d53c7266dddfa89f0
Pavan Kunapuli <pkunapuli@nvidia.com>
2016-12-07 23:23:42 +0530

    mmc: Add HS400 support
    Added support for dll calibration required to be run in HS400 mode.
    Added support for enhanced strobe mode configuration in HS400 mode.
    
    Bug 200241259
    
    Change-Id: I401740a1dbbc86dd6bae0e5b99726c979d5515cc
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/1268423

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -575,6 +575,8 @@ struct sdhci_ops {
 		int signal_voltage);
 	void	(*voltage_switch_post)(struct sdhci_host *host,
 		int signal_voltage);
+	void	(*hs400_enhanced_strobe)(struct sdhci_host *host, bool enable);
+	void	(*post_init)(struct sdhci_host *host);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
