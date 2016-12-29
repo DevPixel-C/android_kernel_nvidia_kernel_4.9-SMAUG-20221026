35bc9f87277f5423e4c9c888ef328b04a629778e
Jon Mayo <jmayo@nvidia.com>
2016-12-28 16:06:32 -0800

    soc/tegra: pmc: fix build errors for CONFIG_PM_SLEEP
    Build errors occur when disabling CONFIG_PM_SLEEP due to undefined
    function set_core_power_timers().
    
    Adds an inline wrapper function to avoid use of unavailable function
    
    Change-Id: Ide6afc0ed3c4159b5131e4978934b10fcb8e8ac8
    Signed-off-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-on: http://git-master/r/1277860
    (cherry picked from commit 4e79f4aa3648dc2da7924b550b9fcf0dced90d93)
    Signed-off-by: Venkat Reddy Talla <vreddytalla@nvidia.com>
    Reviewed-on: http://git-master/r/1280792
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Tested-by: Rakesh Babu Bodla <rbodla@nvidia.com>

--- drivers/soc/tegra/pmc.c
+++ drivers/soc/tegra/pmc.c
@@ -1868,6 +1868,9 @@ void tegra_pmc_enter_suspend_mode(enum tegra_suspend_mode mode)
 	value |= PMC_CNTRL_CPU_PWRREQ_OE;
 	tegra_pmc_writel(value, PMC_CNTRL);
 }
+
+#else
+static inline void set_core_power_timers(void) { }
 #endif
 
 /* IO Pads configurations */
