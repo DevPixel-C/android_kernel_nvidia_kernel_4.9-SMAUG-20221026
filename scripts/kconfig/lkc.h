235ffd0a1fd1f7504bfe609d47dd4cbe9713e228
Stephen Warren <swarren@nvidia.com>
2017-10-16 12:04:05 -0600

    kconfig: implement overlays
    Kernel overlays allow the kernel source tree to be sharded into separate
    directories, referred to as overlays, which are combined into a single
    kernel image during the build process. The overlays can be used e.g. to
    house different sets of IP that are released on different schedules. This
    approach doesn't require the base kernel Kconfig files and Makefiles to
    manually specify when to reference source from the overlays; rather the
    build system automatically combines the overlays.
    
    Bug 1978395
    
    Change-Id: Ic2de5fd3c56a7037dcc6996b990b356050077fe1
    Signed-off-by: Stephen Warren <swarren@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1579977
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- scripts/kconfig/lkc.h
+++ scripts/kconfig/lkc.h
@@ -74,7 +74,7 @@ extern int zconfdebug;
 int zconfparse(void);
 void zconfdump(FILE *out);
 void zconf_starthelp(void);
-FILE *zconf_fopen(const char *name);
+FILE *zconf_fopen(const char *name, int init_overlay_id, int *found_overlay_id, const char **found_name);
 void zconf_initscan(const char *name);
 void zconf_nextfile(const char *name);
 int zconf_lineno(void);
@@ -121,7 +121,7 @@ void menu_finalize(struct menu *parent);
 void menu_set_type(int type);
 
 /* util.c */
-struct file *file_lookup(const char *name);
+struct file *file_lookup(const char *name, int overlay_id, const char *logical_name);
 int file_write_dep(const char *name);
 void *xmalloc(size_t size);
 void *xcalloc(size_t nmemb, size_t size);
