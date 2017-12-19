ffba6a8d0a638bef01828b5f2d90cb5997d25c0d
Laxman Dewangan <ldewangan@nvidia.com>
2017-12-19 22:59:55 +0530

    pinctrl: tegra: Do not add gpio range if gpio node have
    If GPIO node have ranges property then then they refer the
    pinctrl node and pinctrl node must have property
    "#gpio-range-cells".
    
    Use this property to find whether GPIO node have gpio-ranges or not.
    
    Change-Id: I51251b0a557c3aa686f0bc9061e80392d40c967c
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1621308
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Tested-by: Alex Waterman <alexw@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/pinctrl/tegra/pinctrl-tegra.c
+++ drivers/pinctrl/tegra/pinctrl-tegra.c
@@ -986,10 +986,13 @@ static struct syscore_ops pinctrl_syscore_ops = {
 	.restore = tegra_pinctrl_resume,
 };
 
-static bool gpio_node_has_range(void)
+static bool gpio_node_has_range(struct device *dev)
 {
 	struct device_node *np;
 	bool has_prop = false;
+
+	if (of_property_read_bool(dev->of_node, "#gpio-range-cells"))
+		return true;
 
 	np = of_find_compatible_node(NULL, NULL, "nvidia,tegra30-gpio");
 	if (!np)
@@ -1135,7 +1138,7 @@ int tegra_pinctrl_probe(struct platform_device *pdev,
 	}
 
 
-	if (!gpio_node_has_range())
+	if (!gpio_node_has_range(&pdev->dev))
 		pinctrl_add_gpio_range(pmx->pctl, &tegra_pinctrl_gpio_range);
 
 	platform_set_drvdata(pdev, pmx);
