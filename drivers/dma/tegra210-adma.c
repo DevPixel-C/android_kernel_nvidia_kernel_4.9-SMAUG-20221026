2432fc5478339f6836c801269b5a9c3bcd1bedda
Uday Gupta <udayg@nvidia.com>
2018-01-22 09:54:17 +0530

    adma: Use raw_spinlock instead of spinlock
    ADMA interrupts run in NON THREADED mode in RT Kernel,
    so need to use raw_spinlock to get the spinlock
    functionality in RT kernel.
    
    Bug 200380207
    
    Change-Id: Ie00400eda489fb623f8fcfa4d40cacd36c3a33e5
    Signed-off-by: Uday Gupta <udayg@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/1669366
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: Jonathan Hunter <jonathanh@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Nitin Pai <npai@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/dma/tegra210-adma.c
+++ drivers/dma/tegra210-adma.c
@@ -706,17 +706,17 @@ static irqreturn_t tegra_adma_isr(int irq, void *dev_id)
 	unsigned long status;
 	unsigned long flags;
 
-	spin_lock_irqsave(&tdc->vc.lock, flags);
+	raw_spin_lock_irqsave(&tdc->vc.lock, flags);
 
 	status = tegra_adma_irq_clear(tdc);
 	if (status == 0 || !tdc->desc) {
-		spin_unlock_irqrestore(&tdc->vc.lock, flags);
+		raw_spin_unlock_irqrestore(&tdc->vc.lock, flags);
 		return IRQ_NONE;
 	}
 
 	vchan_cyclic_callback(&tdc->desc->vd);
 
-	spin_unlock_irqrestore(&tdc->vc.lock, flags);
+	raw_spin_unlock_irqrestore(&tdc->vc.lock, flags);
 
 	return IRQ_HANDLED;
 }
@@ -726,14 +726,14 @@ static void tegra_adma_issue_pending(struct dma_chan *dc)
 	struct tegra_adma_chan *tdc = to_tegra_adma_chan(dc);
 	unsigned long flags;
 
-	spin_lock_irqsave(&tdc->vc.lock, flags);
+	raw_spin_lock_irqsave(&tdc->vc.lock, flags);
 
 	if (vchan_issue_pending(&tdc->vc)) {
 		if (!tdc->desc)
 			tegra_adma_start(tdc);
 	}
 
-	spin_unlock_irqrestore(&tdc->vc.lock, flags);
+	raw_spin_unlock_irqrestore(&tdc->vc.lock, flags);
 }
 
 static int tegra_adma_is_paused(struct tegra_adma_chan *tdc)
@@ -785,14 +785,14 @@ static int tegra_adma_terminate_all(struct dma_chan *dc)
 	unsigned long flags;
 	LIST_HEAD(head);
 
-	spin_lock_irqsave(&tdc->vc.lock, flags);
+	raw_spin_lock_irqsave(&tdc->vc.lock, flags);
 
 	if (tdc->desc)
 		tegra_adma_stop(tdc);
 
 	tegra_adma_request_free(tdc);
 	vchan_get_all_descriptors(&tdc->vc, &head);
-	spin_unlock_irqrestore(&tdc->vc.lock, flags);
+	raw_spin_unlock_irqrestore(&tdc->vc.lock, flags);
 	vchan_dma_desc_free_list(&tdc->vc, &head);
 
 	return 0;
@@ -813,7 +813,7 @@ static enum dma_status tegra_adma_tx_status(struct dma_chan *dc,
 	if (ret == DMA_COMPLETE || !txstate)
 		return ret;
 
-	spin_lock_irqsave(&tdc->vc.lock, flags);
+	raw_spin_lock_irqsave(&tdc->vc.lock, flags);
 
 	vd = vchan_find_desc(&tdc->vc, cookie);
 	if (vd) {
@@ -825,7 +825,7 @@ static enum dma_status tegra_adma_tx_status(struct dma_chan *dc,
 		residual = 0;
 	}
 
-	spin_unlock_irqrestore(&tdc->vc.lock, flags);
+	raw_spin_unlock_irqrestore(&tdc->vc.lock, flags);
 
 	dma_set_residue(txstate, residual);
 
