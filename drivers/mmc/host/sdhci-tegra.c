madisongh/linux-tegra

mmc: tegra: add platform data to enable/disable periodic cache flush
Added platform data member for enabling/disabling
periodic cache flush support for eMMC device.
Parse periodic cache flush timeout value from dt.

Bug 200165213

Change-Id: I9e13637e20fadffb0183c4d089e167fa6c474f1a
Reviewed-on: http://git-master/r/931589

Signed-off-by: R Raj Kumar <rrajk@nvidia.com>
Change-Id: I15c6477ea5b5703b8801d1a746fc762ddf8c42d4
Reviewed-on: http://git-master/r/1171080
GVS: Gerrit_Virtual_Submit
Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

drivers/mmc/host/sdhci-tegra.c
@@ -1326,6 +1326,19 @@ static int sdhci_tegra_parse_dt(struct device *dev)
 	plat->enable_hw_cq =
 		of_property_read_bool(np, "nvidia,enable-hwcq");
 #endif
+	plat->en_periodic_cflush = of_property_read_bool(np,
+			"nvidia,en-periodic-cflush");
+	if (plat->en_periodic_cflush) {
+		val = 0;
+		of_property_read_u32(np, "nvidia,periodic-cflush-to", &val);
+		host->mmc->flush_timeout = val;
+		if (val == 0) {
+			plat->en_periodic_cflush = false;
+			dev_warn(dev, "Periodic cache flush feature disabled,"
+				"since flush timeout value is zero.\n");
+		}
+	}
+
 	tegra_host->plat = plat;
 	return mmc_of_parse(host->mmc);
 }
@@ -1477,6 +1490,9 @@ static int sdhci_tegra_probe(struct platform_device *pdev)
 	}
 	if (plat->en_strobe)
 		host->mmc->caps2 |= MMC_CAP2_EN_STROBE;
+
+	if (plat->en_periodic_cflush)
+		host->mmc->caps2 |= MMC_CAP2_PERIODIC_CACHE_FLUSH;
 
 #ifdef CONFIG_MMC_CQ_HCI
 	if (plat->enable_hw_cq) {

DevPixel-C/android_kernel_nvidia_kernel_4.9-SMAUG-20221026

Add platform data member for enabling/disabling
cherry-picked from commit "8cec7e2450133602980446ada770e8e6ab1448d8" and
aligned the change with K4.9
Bug 200325719
Reviewed-on: https://git-master/r/1514355
Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>

drivers/mmc/host/sdhci-tegra.c
@@ -185,6 +185,7 @@ struct sdhci_tegra {
 	struct pinctrl *pinctrl_sdmmc;
 	struct pinctrl_state *e_33v_enable;
 	struct pinctrl_state *e_33v_disable;
+	bool en_periodic_cflush; /* Enable periodic cache flush for eMMC */
 };
 
 /* Module params */
@@ -1428,6 +1429,20 @@ static int sdhci_tegra_parse_dt(struct platform_device *pdev)
 	tegra_host->rate_change_needs_clk = of_property_read_bool(np,
 		"nvidia,rate-change-needs-clock-enabled");
 
+	tegra_host->en_periodic_cflush = of_property_read_bool(np,
+			"nvidia,en-periodic-cflush");
+	if (tegra_host->en_periodic_cflush) {
+		val = 0;
+		of_property_read_u32(np, "nvidia,periodic-cflush-to", &val);
+		host->mmc->flush_timeout = val;
+		if (val == 0) {
+			tegra_host->en_periodic_cflush = false;
+			dev_warn(&pdev->dev,
+				 "Periodic cache flush feature disabled,"
+				 "since flush timeout value is zero.\n");
+		}
+	}
+
 	return 0;
 }
 
@@ -1530,6 +1545,9 @@ static int sdhci_tegra_probe(struct platform_device *pdev)
 
 	if (!en_boot_part_access)
 		host->mmc->caps2 |= MMC_CAP2_BOOTPART_NOACC;
+
+	if (tegra_host->en_periodic_cflush)
+		host->mmc->caps2 |= MMC_CAP2_PERIODIC_CACHE_FLUSH;
 
 #ifdef CONFIG_MMC_CQ_HCI
 	if (tegra_host->enable_hwcq) {