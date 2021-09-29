OE4T/linux-tegra-4.9

mmc: sdio: Support only 1.8V signal voltage
Some host controller/bus configurations only support 1.8V
signal voltage. Force 1.8V signal voltage for such instances.

Bug 200241259

Change-Id: I0d739d24198f3feacfa4df887cf1221d4ee7383b
Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
Reviewed-on: http://git-master/r/1326844
GVS: Gerrit_Virtual_Submit
Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
Reviewed-by: Timo Alho <talho@nvidia.com>

drivers/mmc/core/host.c
@@ -319,6 +319,8 @@ int mmc_of_parse(struct mmc_host *host)
 		host->caps2 |= MMC_CAP2_NO_SD;
 	if (of_property_read_bool(np, "no-mmc"))
 		host->caps2 |= MMC_CAP2_NO_MMC;
+	if (of_property_read_bool(np, "only-1-8-v"))
+		host->caps2 |= MMC_CAP2_ONLY_1V8_SIGNAL_VOLTAGE;
 
 	host->dsr_req = !of_property_read_u32(np, "dsr", &host->dsr);
 	if (host->dsr_req && (host->dsr & ~0xffff)) {

DevPixel-C/android_kernel_nvidia_kernel_4.9-SMAUG-20221026

drivers/mmc/core/host.c
@@ -317,6 +317,8 @@ int mmc_of_parse(struct mmc_host *host)
 		host->caps2 |= MMC_CAP2_NO_SD;
 	if (device_property_read_bool(dev, "no-mmc"))
 		host->caps2 |= MMC_CAP2_NO_MMC;
+	if (device_property_read_bool(dev, "only-1-8-v"))
+		host->caps2 |= MMC_CAP2_ONLY_1V8_SIGNAL_VOLTAGE;
 
 	host->dsr_req = !device_property_read_u32(dev, "dsr", &host->dsr);
 	if (host->dsr_req && (host->dsr & ~0xffff)) {