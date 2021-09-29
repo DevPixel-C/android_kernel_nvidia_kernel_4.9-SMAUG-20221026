madisongh/linux-tegra

tegra: fuse: set proper chipid/revision through module params
Set tegra_chip_id/revision module sysfs entry correctly if sysfs
entry is read before chip_id/revision is initialized.

This support is added as part of kernel unification of fuse driver.

Bug 200260692

Change-Id: I018729c20cc0b7bb27c69f80ff978d303a1b0c91
Signed-off-by: Shardar Shariff Md <smohammed@nvidia.com>
Reviewed-on: http://git-master/r/1282862
GVS: Gerrit_Virtual_Submit
Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

drivers/soc/tegra/fuse/tegra-chipid.c
@@ -86,11 +86,17 @@ module_param_cb(tegra_cpu, &tegra_cpu_ops, &tegra_cpu_ptr, 0444);
 
 static int get_chip_id(char *val, const struct kernel_param *kp)
 {
+	if (tegra_id.chipid == TEGRA_CHIPID_UNKNOWN)
+		tegra_set_tegraid_from_hw();
+
 	return param_get_uint(val, kp);
 }
 
 static int get_revision(char *val, const struct kernel_param *kp)
 {
+	if (tegra_id.revision == TEGRA_CHIPID_UNKNOWN)
+		tegra_set_tegraid_from_hw();
+
 	return param_get_uint(val, kp);
 }
 

DevPixel-C/android_kernel_nvidia_kernel_4.9-SMAUG-20221026

(cherry picked from commit 25c7aae8c301882f4b0637d9719b214466d1ac1f)
Reviewed-on: http://git-master/r/1319722

drivers/soc/tegra/fuse/tegra-chipid.c
@@ -86,11 +86,17 @@ module_param_cb(tegra_cpu, &tegra_cpu_ops, &tegra_cpu_ptr, 0444);
 
 static int get_chip_id(char *val, const struct kernel_param *kp)
 {
+	if (tegra_id.chipid == TEGRA_CHIPID_UNKNOWN)
+		tegra_set_tegraid_from_hw();
+
 	return param_get_uint(val, kp);
 }
 
 static int get_revision(char *val, const struct kernel_param *kp)
 {
+	if (tegra_id.revision == TEGRA_REVISION_UNKNOWN)
+		tegra_set_tegraid_from_hw();
+
 	return param_get_uint(val, kp);
 }
 