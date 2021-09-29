InES-HPMM/linux-l4t

spi: tegra: set default chip select
Set default chip select to single slave connected or
to slave which has default-cs in its dt entry

Bug 1540684

Change-Id: I33850ac21fd641a5950c4ab280b4e93450cf01da
Signed-off-by: Krishna Yarlagadda <kyarlagadda@nvidia.com>
Reviewed-on: http://git-master/r/498815
Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
Tested-by: Laxman Dewangan <ldewangan@nvidia.com>

drivers/spi/spi-tegra114.c
@@ -229,6 +229,7 @@ struct tegra_spi_data {
 	u32					def_command1_reg;
 	u32					def_command2_reg;
 	u32					spi_cs_timing;
+	u8					def_chip_select;
 
 	struct completion			xfer_completion;
 	struct spi_transfer			*curr_xfer;
@@ -1362,6 +1363,9 @@ static struct tegra_spi_platform_data *tegra_spi_parse_dt(
 	struct tegra_spi_platform_data *pdata;
 	const unsigned int *prop;
 	struct device_node *np = pdev->dev.of_node;
+	struct device_node *nc = NULL;
+	struct device_node *found_nc = NULL;
+	int len;
 
 	pdata = devm_kzalloc(&pdev->dev, sizeof(*pdata), GFP_KERNEL);
 	if (!pdata) {
@@ -1375,6 +1379,27 @@ static struct tegra_spi_platform_data *tegra_spi_parse_dt(
 
 	if (of_find_property(np, "nvidia,clock-always-on", NULL))
 		pdata->is_clkon_always = true;
+
+	/* when no client is defined, default chipselect is zero */
+	pdata->def_chip_select = 0;
+
+	/*
+	 * Last child node or first node which has property as default-cs will
+	 * become the default.
+	 */
+	for_each_available_child_of_node(np, nc) {
+		found_nc = nc;
+		if (of_find_property(nc, "nvidia,default-chipselect", NULL))
+			break;
+	}
+	if (found_nc) {
+		prop = of_get_property(found_nc, "reg", &len);
+		if (!prop || len < sizeof(*prop))
+			dev_err(&pdev->dev, "%s has no reg property\n",
+					found_nc->full_name);
+		else
+			pdata->def_chip_select = be32_to_cpup(prop);
+	}
 
 	return pdata;
 }
@@ -1463,6 +1488,7 @@ static int tegra_spi_probe(struct platform_device *pdev)
 	tspi = spi_master_get_devdata(master);
 	tspi->master = master;
 	tspi->clock_always_on = pdata->is_clkon_always;
+	tspi->def_chip_select = pdata->def_chip_select;
 	tspi->dev = &pdev->dev;
 
 	if (pdev->dev.of_node) {
@@ -1543,6 +1569,7 @@ static int tegra_spi_probe(struct platform_device *pdev)
 		goto exit_pm_disable;
 	}
 	tspi->def_command1_reg  = SPI_M_S | SPI_LSBYTE_FE;
+	tspi->def_command1_reg |= SPI_CS_SEL(tspi->def_chip_select);
 	tegra_spi_writel(tspi, tspi->def_command1_reg, SPI_COMMAND1);
 	tspi->def_command2_reg = tegra_spi_readl(tspi, SPI_COMMAND2);
 	pm_runtime_put(&pdev->dev);

DevPixel-C/android_kernel_nvidia_kernel_4.9-SMAUG-20221026

Set default chip select to single/last slave connected or
to slave which has default-cs in its dt entry. This would
avoid additional register write during transfers for that
device.
Bug 1731138
(cherry picked from commit 8d9ab9717f9bead59546daf799973aab6ae0c023)
Reviewed-on: http://git-master/r/1165021

drivers/spi/spi-tegra114.c
@@ -69,6 +69,7 @@
 #define SPI_CONTROL_MODE_3			(3 << 28)
 #define SPI_CONTROL_MODE_MASK			(3 << 28)
 #define SPI_MODE_SEL(x)				(((x) & 0x3) << 28)
+#define SPI_MODE_VAL(x)				(((x) >> 28) & 0x3)
 #define SPI_M_S					(1 << 30)
 #define SPI_PIO					(1 << 31)
 
@@ -232,6 +233,7 @@ struct tegra_spi_data {
 	u32					spi_cs_timing;
 	u32					def_command2_reg;
 	u8					last_used_cs;
+	u8					def_chip_select;
 
 	struct completion			xfer_completion;
 	struct spi_transfer			*curr_xfer;
@@ -883,7 +885,9 @@ static u32 tegra_spi_setup_transfer_one(struct spi_device *spi,
 				tegra_spi_writel(tspi, command1, SPI_COMMAND1);
 			tspi->cs_control = NULL;
 		} else
-			tegra_spi_writel(tspi, command1, SPI_COMMAND1);
+			if (SPI_MODE_VAL(command1) !=
+			    SPI_MODE_VAL(tspi->def_command1_reg))
+				tegra_spi_writel(tspi, command1, SPI_COMMAND1);
 
 		tspi->is_hw_based_cs = false;
 		if (cdata && cdata->is_hw_based_cs && is_single_xfer &&
@@ -1071,6 +1075,8 @@ static int tegra_spi_setup(struct spi_device *spi)
 		val &= ~SPI_CS_POL_INACTIVE(spi->chip_select);
 	else
 		val |= SPI_CS_POL_INACTIVE(spi->chip_select);
+	if (tspi->def_chip_select == spi->chip_select)
+		val |= SPI_MODE_SEL(spi->mode & 0x3);
 	tspi->def_command1_reg = val;
 	tegra_spi_writel(tspi, tspi->def_command1_reg, SPI_COMMAND1);
 	spin_unlock_irqrestore(&tspi->lock, flags);
@@ -1378,10 +1384,37 @@ static irqreturn_t tegra_spi_isr(int irq, void *context_data)
 
 static void tegra_spi_parse_dt(struct tegra_spi_data *tspi)
 {
+	const unsigned int *prop;
 	struct device_node *np = tspi->dev->of_node;
+	struct device_node *nc = NULL;
+	struct device_node *found_nc = NULL;
+	int len;
+	int ret;
 
 	if (of_find_property(np, "nvidia,clock-always-on", NULL))
 		tspi->clock_always_on = true;
+
+	/*
+	 * Last child node or first node which has property as default-cs will
+	 * become the default. When no client is defined, default chipselect
+	 * is zero.
+	 */
+	tspi->def_chip_select = 0;
+
+	for_each_available_child_of_node(np, nc) {
+		found_nc = nc;
+		ret = of_property_read_bool(nc, "nvidia,default-chipselect");
+		if (ret)
+			break;
+	}
+	if (found_nc) {
+		prop = of_get_property(found_nc, "reg", &len);
+		if (!prop || len < sizeof(*prop))
+			dev_err(tspi->dev, "%s has no reg property\n",
+				found_nc->full_name);
+		else
+			tspi->def_chip_select = be32_to_cpup(prop);
+	}
 }
 
 static struct tegra_spi_chip_data tegra114_spi_chip_data = {
@@ -1531,6 +1564,7 @@ static int tegra_spi_probe(struct platform_device *pdev)
 	reset_control_reset(tspi->rst);
 
 	tspi->def_command1_reg  = SPI_M_S | SPI_LSBYTE_FE;
+	tspi->def_command1_reg |= SPI_CS_SEL(tspi->def_chip_select);
 	tegra_spi_writel(tspi, tspi->def_command1_reg, SPI_COMMAND1);
 	tspi->def_command2_reg = tegra_spi_readl(tspi, SPI_COMMAND2);
 	pm_runtime_put(&pdev->dev);