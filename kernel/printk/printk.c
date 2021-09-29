InES-HPMM/linux-l4t

printk: add loglevel for messages from /dev/kmsg
Bug 1644887

Allow messages to /dev/kmsg to have a different loglevel other than
the default_message_loglevel, which is the one kernel printk() uses
when no loglevel is specified.

Change-Id: I340d20693d87cfea9aaea51845caf32b4cbd62fa
Signed-off-by: Eric Miao <emiao@nvidia.com>
Reviewed-on: http://git-master/r/756603
Reviewed-by: Li Li (SW-TEGRA) <lli5@nvidia.com>
Tested-by: Li Li (SW-TEGRA) <lli5@nvidia.com>
Reviewed-by: Nitin Kumbhar <nkumbhar@nvidia.com>
Reviewed-on: http://git-master/r/805255
GVS: Gerrit_Virtual_Submit
Reviewed-by: Vinayak Pane <vpane@nvidia.com>

kernel/printk.c
@@ -62,11 +62,12 @@ extern void printascii(char *);
 #define MINIMUM_CONSOLE_LOGLEVEL 1 /* Minimum loglevel we let people use */
 #define DEFAULT_CONSOLE_LOGLEVEL 7 /* anything MORE serious than KERN_DEBUG */
 
-int console_printk[4] = {
+int console_printk[5] = {
 	DEFAULT_CONSOLE_LOGLEVEL,	/* console_loglevel */
 	DEFAULT_MESSAGE_LOGLEVEL,	/* default_message_loglevel */
 	MINIMUM_CONSOLE_LOGLEVEL,	/* minimum_console_loglevel */
 	DEFAULT_CONSOLE_LOGLEVEL,	/* default_console_loglevel */
+	DEFAULT_MESSAGE_LOGLEVEL,	/* default_devkmsg_loglevel */
 };
 
 /*
@@ -428,7 +429,7 @@ static ssize_t devkmsg_writev(struct kiocb *iocb, const struct iovec *iv,
 {
 	char *buf, *line;
 	int i;
-	int level = default_message_loglevel;
+	int level = default_devkmsg_loglevel;
 	int facility = 1;	/* LOG_USER */
 	size_t len = iov_length(iv, count);
 	ssize_t ret = len;

DevPixel-C/android_kernel_nvidia_kernel_4.9-SMAUG-20221026

Bug 200411989
This allows existing QA scripts which write to /dev/kmsg can still
be routed to UART despite the global console loglevel being lowered.
(cherry picked from commit 12e5d788cb6eaac1c57492b09397095b7b40adce)
Signed-off-by: Kary Jin <karyj@nvidia.com>
Reviewed-on: https://git-master.nvidia.com/r/1743488
Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
Reviewed-by: Nicolin Chen <nicolinc@nvidia.com>
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

kernel/printk/printk.c
@@ -60,11 +60,12 @@
 extern void printascii(char *);
 #endif
 
-int console_printk[4] = {
+int console_printk[5] = {
 	CONSOLE_LOGLEVEL_DEFAULT,	/* console_loglevel */
 	MESSAGE_LOGLEVEL_DEFAULT,	/* default_message_loglevel */
 	CONSOLE_LOGLEVEL_MIN,		/* minimum_console_loglevel */
 	CONSOLE_LOGLEVEL_DEFAULT,	/* default_console_loglevel */
+	MESSAGE_LOGLEVEL_DEFAULT,	/* default_devkmsg_loglevel */
 };
 
 /*
@@ -713,7 +714,7 @@ struct devkmsg_user {
 static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 {
 	char *buf, *line;
-	int level = default_message_loglevel;
+	int level = default_devkmsg_loglevel;
 	int facility = 1;	/* LOG_USER */
 	struct file *file = iocb->ki_filp;
 	struct devkmsg_user *user = file->private_data;