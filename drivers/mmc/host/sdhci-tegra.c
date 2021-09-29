madisongh/linux-tegra

mmc: tegra: Change clock rate before enabling clock
Frequency configuration should be done before enabling clock.
T186 SDMMC4 controller is the exception to this where clock
needs to be enabled for changing frequency. Added support to
take care of this condition.

Bug 200267453

Change-Id: I070ac7845f18740cd8939d8359c0f21d465d1df9
Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
Reviewed-on: http://git-master/r/1279529
Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

drivers/mmc/host/sdhci-tegra.c
@@ -176,9 +176,10 @@ struct sdhci_tegra {
 	const struct sdhci_tegra_soc_data *soc_data;
 	const struct tegra_sdhci_platform_data *plat;
 	struct tegra_bwmgr_client *emc_clk;
-	bool	clk_enabled;
+	bool clk_enabled;
+	bool rate_change_needs_clk;
 	/* ensure atomic set clock calls */
-	struct mutex	set_clock_mutex;
+	struct mutex set_clock_mutex;
 	/* max clk supported by the platform */
 	unsigned int max_clk_limit;
 	/* max ddr clk supported by the platform */
@@ -743,6 +744,8 @@ static void tegra_sdhci_set_clock(struct sdhci_host *sdhci, unsigned int clock)
 	pr_debug("%s %s %u enabled=%u\n", __func__,
 		mmc_hostname(sdhci->mmc), clock, tegra_host->clk_enabled);
 	if (clock) {
+		if (!plat->rate_change_needs_clk)
+			tegra_sdhci_set_clk_rate(sdhci, clock);
 		if (!tegra_host->clk_enabled) {
 			ret = clk_prepare_enable(pltfm_host->clk);
 			if (ret) {
@@ -766,7 +769,8 @@ static void tegra_sdhci_set_clock(struct sdhci_host *sdhci, unsigned int clock)
 					ret);
 			}
 		}
-		tegra_sdhci_set_clk_rate(sdhci, clock);
+		if (plat->rate_change_needs_clk)
+			tegra_sdhci_set_clk_rate(sdhci, clock);
 		if (plat->en_periodic_calib &&
 			sdhci->is_calibration_done) {
 			cur_time = ktime_get();
@@ -1698,6 +1702,8 @@ static int sdhci_tegra_parse_dt(struct device *dev)
 	plat->cd_wakeup_capable = of_property_read_bool(np, "nvidia,cd-wakeup-capable");
 	plat->en_periodic_calib = of_property_read_bool(np,
 			"nvidia,en-periodic-calib");
+	plat->rate_change_needs_clk = of_property_read_bool(np,
+		"nvidia,rate-change-needs-clock-enabled");
 
 	tegra_host->plat = plat;
 	return mmc_of_parse(host->mmc);

DevPixel-C/android_kernel_nvidia_kernel_4.9-SMAUG-20221026

cherry-picked from commit "aef9f0ed1e55ebe6a7ce7134ddf6e690b4851b97"
Signed-off-by: R Raj Kumar <rrajk@nvidia.com>
Reviewed-on: http://git-master/r/1480723
GVS: Gerrit_Virtual_Submit
Reviewed-by: Venu Byravarasu <vbyravarasu@nvidia.com>

drivers/mmc/host/sdhci-tegra.c
@@ -166,6 +166,7 @@ struct sdhci_tegra {
 	unsigned long max_ddr_clk_limit;
 	struct tegra_prod *prods;
 	u8 tuned_tap_delay;
+	bool rate_change_needs_clk;
 	unsigned int tuning_status;
 	#define TUNING_STATUS_DONE	1
 	#define TUNING_STATUS_RETUNE	2
@@ -757,6 +758,8 @@ static void tegra_sdhci_set_clock(struct sdhci_host *host, unsigned int clock)
 	host_clk = tegra_sdhci_apply_clk_limits(host, clock);
 
 	if (clock) {
+		if (!tegra_host->rate_change_needs_clk)
+			tegra_sdhci_set_clk_rate(host, clock);
 		/* Enable SDMMC host CAR clock */
 		if (!tegra_host->is_clk_enabled) {
 			rc = clk_prepare_enable(pltfm_host->clk);
@@ -776,7 +779,8 @@ static void tegra_sdhci_set_clock(struct sdhci_host *host, unsigned int clock)
 		}
 
 		/* Set the desired clk freq rate */
-		tegra_sdhci_set_clk_rate(host, host_clk);
+		if (tegra_host->rate_change_needs_clk)
+			tegra_sdhci_set_clk_rate(host, host_clk);
 		host->max_clk = clk_get_rate(pltfm_host->clk);
 		dev_dbg(mmc_dev(host->mmc), "req clk %lu, set clk %d\n",
 			host_clk, host->max_clk);
@@ -1416,6 +1420,9 @@ static int sdhci_tegra_parse_dt(struct platform_device *pdev)
 		else if (val == 3)
 			host->ocr_mask &= (MMC_VDD_33_34 | MMC_VDD_165_195);
 	}
+	tegra_host->rate_change_needs_clk = of_property_read_bool(np,
+		"nvidia,rate-change-needs-clock-enabled");
+
 	return 0;
 }
 